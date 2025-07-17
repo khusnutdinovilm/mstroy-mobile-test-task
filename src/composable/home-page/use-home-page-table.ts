ef63467a3a8e5789901e75538f680858464552f8
ilmir khusnutdinov <ilmir.khusnutdinov.kh@gmail.com>
2025-07-17 21:07:04 +0500

    ADD: разработал composable-функцию для работы с таблицей tree-table

---
+++ src/composable/home-page/use-home-page-table.ts
@@ -0,0 +1,63 @@
+import { computed, ref } from "vue";
+
+import type { ColDef } from "ag-grid-enterprise";
+
+import TreeCell from "src/modules/tree-store/components/tree-cell";
+
+import useActiveTreeStore from "src/modules/tree-store/store/use-active-tree-store";
+import useTreeMode from "src/modules/tree-store/store/use-tree-mode";
+
+import type { INode } from "src/modules/tree-store/types/tree-store";
+
+export default function () {
+  const treeModeStore = useTreeMode();
+  const treeTableStore = useActiveTreeStore();
+
+  const tableColumns = ref<ColDef[]>([
+    {
+      field: "label",
+      headerName: "Наименование",
+      editable: () => treeModeStore.isEditMode,
+      resizable: false,
+    },
+  ]);
+
+  const tableRows = computed(() => {
+    if (!treeTableStore.treeStore) return [];
+
+    const allNodes = treeTableStore.treeStore.getAll();
+
+    return allNodes.map(node => {
+      const allParents = treeTableStore.treeStore!.getAllParents(node.id);
+
+      return {
+        ...node,
+        path: allParents.map(node => node.id).reverse(),
+      };
+    });
+  });
+
+  const autoGroupColumnDef = ref<ColDef>({
+    headerName: "Категория",
+    field: "id",
+    resizable: false,
+    cellRenderer: TreeCell,
+    cellRendererParams: {
+      suppressCount: true,
+      isEditMode: () => treeModeStore.isEditMode,
+      createNewNode: treeTableStore.addNewNode,
+      deleteNode: treeTableStore.removeNode,
+    },
+  });
+
+  const editNode = ({ id, parent, label }: INode) => {
+    treeTableStore.updateNode({ id, parent, label });
+  };
+
+  return {
+    tableColumns,
+    tableRows,
+    autoGroupColumnDef,
+    editNode,
+  };
+}
