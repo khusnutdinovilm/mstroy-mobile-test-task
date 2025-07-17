5f6e4c25fe70b7696b6807e7751bed98e99eae43
ilmir khusnutdinov <ilmir.khusnutdinov.kh@gmail.com>
2025-07-17 05:12:07 +0500

    UPD: добавил в компонент tree-mode-toggle состояние disable , написал стили для данного состояния и добавил оявление уведомления при переключении режима

--- src/modules/tree-store/components/tree-mode-toggle/tree-mode-toggle.vue
+++ src/modules/tree-store/components/tree-mode-toggle/tree-mode-toggle.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tree-mode-toggle" @click="treeModeStore.toggleMode">
+  <div class="tree-mode-toggle" :class="treeModeClasses" @click="toggleMode">
     <div class="tree-mode-toggle__label" :title="`Переключить в режим ${nextLabel}`">
       Режим {{ currentLabel }}
     </div>
@@ -10,6 +10,7 @@
         round
         :icon="currentIcon"
         color="primary"
+        :disable="disabled"
         :aria-label="`Режим ${currentLabel}`"
       />
     </div>
@@ -19,11 +20,22 @@
 <script setup lang="ts">
 import { computed } from "vue";
 
+import { useQuasar } from "quasar";
 import useTreeMode from "src/modules/tree-store/store/use-tree-mode";
 
 defineOptions({
   name: "tree-mode-toggle",
 });
+
+const $q = useQuasar();
+
+const props = defineProps<{
+  disabled?: boolean;
+}>();
+
+const treeModeClasses = computed(() => ({
+  "tree-mode-toggle--disable": props.disabled,
+}));
 
 const treeModeStore = useTreeMode();
 
@@ -32,15 +44,36 @@ const isEdit = computed(() => treeModeStore.isEditMode);
 const currentLabel = computed(() => (isEdit.value ? "редактирования" : "просмотра"));
 const nextLabel = computed(() => (!isEdit.value ? "редактирования" : "просмотра"));
 const currentIcon = computed(() => (isEdit.value ? "edit" : "visibility"));
+
+function toggleMode() {
+  treeModeStore.toggleMode();
+  $q.notify({
+    icon: "",
+    type: "positive",
+    position: "bottom-right",
+    message: `Вы переключись в режим <strong>${currentLabel.value}</strong>`,
+    html: true,
+    timeout: 500,
+  });
+}
 </script>
 
 <style lang="scss">
 .tree-mode-toggle {
+  --label-color: #{$blue-10};
+  --label-cursor: pointer;
+
+  &--disable {
+    --label-color: #{$blue-3};
+    --label-cursor: not-allowed;
+    pointer-events: none;
+  }
+
   &__label {
     display: none;
-    cursor: pointer;
+    cursor: var(--label-cursor);
     text-decoration: underline;
-    color: $primary;
+    color: var(--label-color);
 
     @media screen and (min-width: 768px) {
       display: block;
