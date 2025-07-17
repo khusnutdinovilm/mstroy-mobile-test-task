9225d9450e2c97c329b145843569ac79d27de119
ilmir khusnutdinov <ilmir.khusnutdinov.kh@gmail.com>
2025-07-17 19:56:56 +0500

    ADD: разработал утилиту для получения содержимого загруженного файла

---
+++ src/modules/dataset/utils/file/read-file-as-text.ts
@@ -0,0 +1,15 @@
+export function readFileAsText(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      resolve(reader.result as string);
+    };
+
+    reader.onerror = () => {
+      reject(new Error("Ошибка чтения файла"));
+    };
+
+    reader.readAsText(file);
+  });
+}
